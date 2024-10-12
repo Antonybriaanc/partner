e88a7c69fd946b0cc7468c8a069d928327744cf7
Antony Castillo <abcy775@gmail.com>
2024-10-12 18:39:41 -0400

    Modificando manifest para modo html

--- partner_extra_custom_sfc/__manifest__.py
+++ partner_extra_custom_sfc/__manifest__.py
@@ -4,6 +4,18 @@
     'version': '0.1',
     'category': 'Custom',
     'summary': 'Adds extra fields to the partner form.',
+    'description': """
+        <h2>Partner Extra Custom SFC</h2>
+        <p>Este módulo añade campos adicionales al formulario de contactos en Odoo, mejorando la gestión de la información.</p>
+        <h3>Características:</h3>
+        <ul>
+            <li>Campo extra 1: Detalles específicos</li>
+            <li>Campo extra 2: Información adicional</li>
+            <li>Integración con el módulo de contactos</li>
+            <li>Mejora de la comunicación a través del módulo de correo</li>
+        </ul>
+        <p>Con este módulo, podrás gestionar mejor la información de tus clientes y contactos, agregando los campos que necesitas para personalizar tu sistema Odoo.</p>
+    """,
     'installable': True,
     'application': False,
     'depends': [
