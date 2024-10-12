68916772c2dfbf07f1abe652cc0f34a2d055fb80
Antony Castillo <abcy775@gmail.com>
2024-10-12 19:01:37 -0400

    Modificando manifest para modo html14

--- partner_extra_custom_sfc/__manifest__.py
+++ partner_extra_custom_sfc/__manifest__.py
@@ -5,7 +5,7 @@
     'category': 'Custom',
     'summary': 'Adds extra fields to the partner form.',
     'description': """
-        Partner Extra Custom SFC
+       <p> Partner Extra Custom SFC</p>
         <p>Este módulo añade campos adicionales al formulario de contactos en Odoo, mejorando la gestión de la información.</p>
         <h3>Características:</h3>
         <ul>
