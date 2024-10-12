8ee7b06a964b44d37bf1a5b629bc54d4b06d1843
Antony Castillo <abcy775@gmail.com>
2024-10-12 18:50:20 -0400

    Modificando manifest para modo html6

--- partner_extra_custom_sfc/__manifest__.py
+++ partner_extra_custom_sfc/__manifest__.py
@@ -5,37 +5,23 @@
     'category': 'Custom',
     'summary': 'Adds extra fields to the partner form.',
     'description': """
-        <h2>Partner Extra Fields</h2>
-        <p>El módulo <strong>Partner Extra Fields</strong> añade campos adicionales para teléfono y correo electrónico en el formulario de contacto de Odoo. Esta extensión es útil para mantener información adicional sobre los socios en un solo lugar.</p>
-        <h3>Características:</h3>
+        <h2>Partner Extra Custom SFC</h2>
+        <p>Este módulo añade campos adicionales para teléfono y correo electrónico en el formulario de contactos en Odoo, mejorando la gestión de la información.</p>
+        <p><strong>Características:</strong></p>
         <ul>
             <li>Añade un campo de teléfono adicional.</li>
             <li>Añade un campo de correo electrónico adicional.</li>
             <li>Se integra con el módulo de contactos existente.</li>
         </ul>
-        <h3>Instalación:</h3>
+        <p><strong>Instalación:</strong></p>
         <ol>
             <li>Clona el repositorio en tu sistema local.</li>
             <li>Copia la carpeta del módulo en el directorio de addons de Odoo.</li>
             <li>Reinicia el servidor de Odoo.</li>
             <li>Ve a la interfaz de Odoo y actualiza la lista de módulos.</li>
-            <li>Busca <strong>Partner Extra Fields</strong> y haz clic en Instalar.</li>
+            <li>Busca "Partner Extra Custom SFC" y haz clic en Instalar.</li>
         </ol>
-        <h3>Uso:</h3>
-        <p>Después de la instalación, los nuevos campos aparecerán en el formulario de contacto. Puedes completar los campos adicionales según sea necesario.</p>
-        <h3>Contribuciones:</h3>
-        <p>Las contribuciones son bienvenidas. Si deseas colaborar en este proyecto, sigue estos pasos:</p>
-        <ol>
-            <li>Haz un fork del repositorio.</li>
-            <li>Crea una nueva rama (<code>git checkout -b feature/nueva-funcionalidad</code>).</li>
-            <li>Realiza tus cambios y confirma los cambios (<code>git commit -m 'Añadida nueva funcionalidad'</code>).</li>
-            <li>Sube tus cambios (<code>git push origin feature/nueva-funcionalidad</code>).</li>
-            <li>Abre una pull request.</li>
-        </ol>
-        <h3>Licencia:</h3>
-        <p>Este proyecto está licenciado bajo la licencia MIT. Consulta el archivo LICENSE para más detalles.</p>
-        <h3>Autor:</h3>
-        <p>SFC C.A - <a href="mailto:abcy775@gmail.com">abcy775@gmail.com</a></p>
+        <p><strong>Autor:</strong> SFC C.A - <a href="mailto:abcy775@gmail.com">abcy775@gmail.com</a></p>
     """,
     'installable': True,
     'application': False,
